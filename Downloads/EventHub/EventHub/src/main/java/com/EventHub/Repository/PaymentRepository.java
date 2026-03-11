a21c37a39e5d582bcdc444366e591dee95200e46
meenakshi-guntreddy <guntreddymeenakshi6715@gmail.com>
2026-03-11 22:22:01 +0530

    Delete Downloads/EventHub/EventHub directory

--- Downloads/EventHub/EventHub/src/main/java/com/EventHub/Repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.EventHub.Repository;
-import com.EventHub.entity.PaymentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
-
-}
