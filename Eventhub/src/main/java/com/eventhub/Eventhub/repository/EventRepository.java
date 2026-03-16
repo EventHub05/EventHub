2785db5e5f5f23f56cd522b9a4e83afbfa988527
K Jahnavi <jahnavikonda2@gmail.com>
2026-03-16 11:02:13 +0530

    Delete Eventhub directory

--- Eventhub/src/main/java/com/eventhub/Eventhub/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eventhub.Eventhub.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.eventhub.Eventhub.entity.Event;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
--- Eventhub/src/main/java/com/eventhub/Eventhub/repository/VenueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eventhub.Eventhub.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.eventhub.Eventhub.entity.Venue;
-
-public interface VenueRepository extends JpaRepository<Venue, Long> {
-}
