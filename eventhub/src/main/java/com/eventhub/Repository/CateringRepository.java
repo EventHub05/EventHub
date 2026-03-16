52978dbfe297941bc13e383ac49dcf9897c85852
K Jahnavi <jahnavikonda2@gmail.com>
2026-03-16 11:03:47 +0530

    Delete eventhub directory

--- eventhub/src/main/java/com/eventhub/Repository/CateringRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventhub.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.eventhub.Entity.Catering;
-
-public interface CateringRepository extends JpaRepository<Catering, Long> {
-
-	}
-
-
